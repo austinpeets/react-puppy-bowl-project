c0ba1c8779909d475d615028654e723e46e0ca53
Austin Pitts <austinpitts@Austins-Laptop.local>
2024-04-20 14:22:11 -0400

    set up api

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [player, setPlayers] = useState([])
+
+  useEffect(() => {
+  
+    async function pullPlayers() {
+      try {
+      const processing = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2401-ftb-mt-web-pt/players')
+      const data = await processing.json()
+      return data
+    } catch (error){
+      console.error('Error pulling players', error)
+    }
+  } 
+  pullPlayers()
+  }
+  )
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      
     </>
   )
 }
