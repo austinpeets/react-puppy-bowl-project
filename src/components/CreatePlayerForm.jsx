ab5dbc31145fc8d23591222bc540ba625d74b71f
berzyvonne <berzyvonne@gmail.com>
2024-04-22 16:09:53 -0400

    Create Player form created

--- src/components/CreatePlayerForm.jsx
+++ src/components/CreatePlayerForm.jsx
@@ -1,7 +1,56 @@
+import { useState } from "react";
+
 export default function CreatePlayer() {
+const [name, setName] = useState('');
+const [breed, setBreed] = useState('');
+const [status, setStatus] = useState('Bench');
+const [image, setImage] = useState('');
+
+const handleSubmit = (e) =>{
+  e.preventDefault();
+}
+  
     return (
-      <div className="CreatePlayer">
-        <h1>Create Player</h1>
+      <>
+      <div>
+      <h2>Create Player</h2><form onSubmit={handleSubmit}>
+        <label>
+          Name:{" "}
+          <input
+            className="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)} />
+        </label>
+        <label>
+          Breed:{" "}
+          <input
+            className="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)} />
+        </label>
+        <label>
+          Status:{" "}
+          <select
+            value={status}
+            onChange={(e) => setStatus(e.target.value)} >
+            <option value="bench">Bench</option>
+            <option value="field">Field</option>
+            </select>
+        </label>
+        <label>
+          Image:{" "}
+          <input
+            className="image"
+            value={image}
+            onChange={(e) => setImage(e.target.value)} />
+        </label>
+        <button>Submit</button>
+        {/* <p>{ name }</p>
+        <p>{ breed }</p>
+        <p>{ status }</p>
+        <p>{ image }</p> */}
+      </form>
       </div>
-    );
-  }
+      </>
+    )
+}
